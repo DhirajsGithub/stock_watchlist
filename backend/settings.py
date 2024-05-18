e71c73faa1c62d11e100df2c35e66716e9846633
DhirajsGithub <borsedhiraj123@gmail.com>
2024-05-18 16:52:54 +0530

    ready to deploy

--- backend/settings.py
+++ backend/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-n$275bf_cd26f#y-j=@(4m5j-aqmj)xl*jia=fw_$*i2zix27k"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 REST_FRAMEWORK = {
